82003d9582d299138463b41d9c883080ccd19148
Thaneshwor <thaneshwor100@gmail.com>
2020-03-30 12:55:44 +0545

    Display Person information on browser

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Person from './components/day1/Person';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Person />
   </React.StrictMode>,
   document.getElementById('root')
 );
