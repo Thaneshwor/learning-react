a4e430edc6682771f841f772ed161e3a969d7093
Thaneshwor <thaneshwor100@gmail.com>
2020-03-30 22:59:08 +0545

    Class component example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/day1/Greet';
+import Welcome from './components/day1/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greet />
+        <Welcome />
       </div>
     )
   }
---
+++ src/components/day1/Welcome.js
@@ -0,0 +1,11 @@
+// class components(stateful/smart/container)
+
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+        return < h1 >Welcome from Class Component</h1 >
+    }
+}
+
+export default Welcome;
