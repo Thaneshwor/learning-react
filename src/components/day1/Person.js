3a05563489760d5dcb4fa2911d328ba7c87c8b87
Thaneshwor <thaneshwor100@gmail.com>
2020-03-30 12:41:12 +0545

    Create folder to store components and add Person component

---
+++ src/components/day1/Person.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo from "./logo.svg";
+// import "src/App.css";
+
+function Person() {
+  return (
+    <div>
+      <h1>Thaneshwor</h1>
+      <p>Your Age: 25</p>
+    </div>
+  );
+}
+
+export default Person;
