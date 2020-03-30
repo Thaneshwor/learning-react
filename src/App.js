419eaf0635bbf8178fed60750d0485ff6fb413eb
Thaneshwor <thaneshwor100@gmail.com>
2020-03-31 00:10:43 +0545

    Props example

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import './App.css';
 import Greet from './components/day1/Greet';
 import Welcome from './components/day1/Welcome';
 import Hello from './components/day1/Hello';
-import JSXtest from './components/day1/JSXtest'
+import JSXtest from './components/day1/JSXtest';
+import Person from './components/day1/Person';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Greet /> */}
-        {/* <Welcome /> */}
+        {/* function components */}
+        <Person name="Burce" heroName='Batman' ><button>Click me</button></Person>
+        <Person name="Clark" heroName='Superman' />
+        <Person name="Diana" heroName='Wonder Woman' />
+
+        {/* class components */}
+        <Welcome name="Burce" heroName='Batman' />
+        <Welcome name="Clark" heroName='Superman' />
+        <Welcome name="Diana" heroName='Wonder Woman' />
         {/* <Hello /> */}
         <JSXtest />
       </div>
