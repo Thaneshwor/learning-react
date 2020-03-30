b1336ab6f5f0669be929638c800f1237835aa88c
Thaneshwor <thaneshwor100@gmail.com>
2020-03-30 23:38:14 +0545

    JSX example

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/day1/Greet';
 import Welcome from './components/day1/Welcome';
+import Hello from './components/day1/Hello';
+import JSXtest from './components/day1/JSXtest'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet />
-        <Welcome />
+        {/* <Greet /> */}
+        {/* <Welcome /> */}
+        {/* <Hello /> */}
+        <JSXtest />
       </div>
     )
   }
---
+++ src/components/day1/Hello.js
@@ -0,0 +1,13 @@
+// React JSX
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div className='dummy-class'>
+            <h1>Hello Thaneshwor</h1>
+        </div>
+    )
+    // return React.createElement('div', { id: 'hello', className: 'dummy-class' }, React.createElement('h1', null, 'hello thanehwor'));
+}
+
+export default Hello;
