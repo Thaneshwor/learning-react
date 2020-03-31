5f6b4cdada9348953df38fe588de93f94d664582
Thaneshwor <thaneshwor100@gmail.com>
2020-03-31 15:42:13 +0545

    EventBinding example

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import './App.css';
 // import JSXtest from './components/day1/JSXtest';
 // import Person from './components/day1/Person';
 // import Message from './components/day1/Message';
-import Counter from './components/day2/Counter';
-import Greet from './components/day2/Greet';
-import Welcome from './components/day2/Welcome';
-import FunctionClick from './components/day2/eventHandling/FunctionClick';
-import UserList from './components/day2/eventHandling/UserList';
-import ClassClick from './components/day2/eventHandling/ClassClick';
-import Toggle from './components/day2/eventHandling/Toggle';
-import Car from './components/day2/Car';
+// import Counter from './components/day2/Counter';
+// import Greet from './components/day2/Greet';
+// import Welcome from './components/day2/Welcome';
+// import FunctionClick from './components/day2/eventHandling/FunctionClick';
+// import UserList from './components/day2/eventHandling/UserList';
+// import ClassClick from './components/day2/eventHandling/ClassClick';
+// import Toggle from './components/day2/eventHandling/Toggle';
+import EventBind from './components/day2/eventBinding/EventBind';
+// import Car from './components/day2/Car';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick />
-        <ClassClick />
-        <Toggle />
-        <UserList />
+        <EventBind />
       </div>
     )
   }
