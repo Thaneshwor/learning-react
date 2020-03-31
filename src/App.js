ea183358b3e3bea048e835bea4963b535a8172fb
Thaneshwor <thaneshwor100@gmail.com>
2020-03-31 13:19:41 +0545

    EventHandling examples

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// import Hello from './components/day1/Hello';
+// import JSXtest from './components/day1/JSXtest';
+// import Person from './components/day1/Person';
+// import Message from './components/day1/Message';
+import Counter from './components/day2/Counter';
 import Greet from './components/day2/Greet';
 import Welcome from './components/day2/Welcome';
-import Hello from './components/day1/Hello';
-import JSXtest from './components/day1/JSXtest';
-import Person from './components/day1/Person';
-import Message from './components/day1/Message';
-import Counter from './components/day2/Counter';
+import FunctionClick from './components/day2/eventHandling/FunctionClick';
+import ClassClick from './components/day2/eventHandling/ClassClick';
+import Toggle from './components/day2/eventHandling/Toggle';
 import Car from './components/day2/Car';
 
 
@@ -15,8 +19,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name='Diana' heroName='Wonder Women' />
-        <Welcome name='Bruce' heroName='Batman' />
+        <FunctionClick />
+        <ClassClick />
+        <Toggle />
       </div>
     )
   }
