c5cdc918f9dfc5752b1b586fa4666e7f4f452d27
Thaneshwor <thaneshwor100@gmail.com>
2020-04-02 10:05:02 +0545

    Render props example

--- src/App.js
+++ src/App.js
@@ -30,17 +30,36 @@ import './App.css';
 // import Table from './components/day3/fragments/Table';
 // import PureComp from './components/day3/pureComponents/PureComp';
 // import ParentComp from './components/day3/pureComponents/ParentComp';
-import Hero from './components/day3/errorBoundaries/Hero';
-import ErrorBoundary from './components/day3/errorBoundaries/ErrorBoundary';
-import ClickCounter from './components/day3/hoc/ClickCounter';
-import HoverCounter from './components/day3/hoc/HoverCounter';
+// import Hero from './components/day3/errorBoundaries/Hero';
+// import ErrorBoundary from './components/day3/errorBoundaries/ErrorBoundary';
+// import ClickCounter from './components/day3/hoc/ClickCounter';
+// import HoverCounter from './components/day3/hoc/HoverCounter';
+import ClickCounter from './components/day4/renderProps/ClickCounter';
+import HoverCounter from './components/day4/renderProps/HoverCounter';
+import User from './components/day4/renderProps/User';
+import Counter from './components/day4/renderProps/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClickCounter name='Thanos' />
+        {/* <ClickCounter />
         <HoverCounter />
+        <User render={(isLoggedIn) => isLoggedIn ? 'Thanos' : 'Guest'} /> */}
+        <Counter
+          render={(count, incrementCount) => (
+            <ClickCounter count={count} incrementCount={incrementCount} />
+
+          )} />
+
+
+        <Counter
+          render={(count, incrementCount) => (
+            <HoverCounter count={count} incrementCount={incrementCount} />
+
+          )} />
+
+
       </div >
     )
   }
